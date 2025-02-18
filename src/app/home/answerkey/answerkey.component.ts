4e31c11e7cbe3ad95a1571b43caf468226c378fa
qs0625 <qdsmith0625@gmail.com>
2025-02-18 01:05:23 -0600

    updated answerkey component

--- src/app/home/answerkey/answerkey.component.ts
+++ src/app/home/answerkey/answerkey.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-answerkey',
   templateUrl: './answerkey.component.html',
   styleUrls: ['./answerkey.component.css']
 })
 export class AnswerkeyComponent implements OnInit {
+  answerKey: { question: string, answer: string, userSelected: String, index: number }[] = [{question: 'question' ,answer: 'answer', userSelected: 'userSelected', index: 0 }];
 
   constructor() { }
 
   ngOnInit(): void {
+    // Initialize the answer key list
+    this.answerKey = [
+      { question: 'What is the capital of France?', answer: 'Paris', userSelected: 'Berlin', index: 0 },
+      { question: 'What is 2 + 2?', answer: '4',  userSelected: '6', index: 1  },
+      { question: 'Who wrote "To Kill a Mockingbird"?', answer: 'Harper Lee',  userSelected: 'Harper Lee', index: 2 },
+      // Add more questions and answers here
+    ];
   }
-
 }
